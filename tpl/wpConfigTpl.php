fcf4d2e507d8b4477d4344da3180457f18cc3440
webin <1014953068@qq.com>
2023-10-26 14:42:10 +0800

    Del tpl

--- tpl/wpConfigTpl.php
+++ tpl/wpConfigTpl.php
@@ -4,19 +4,10 @@ return <<<TPL
 <?php
 /**
  * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the web site, you can copy this file to "wp-config.php"
- * and fill in the values.
- *
- * This file contains the following configurations:
- *
  * * Database settings
  * * Secret keys
  * * Database table prefix
  * * ABSPATH
- *
- * @link https://wordpress.org/support/article/editing-wp-config-php/
  *
  * @package WordPress
  */
