1245c69ec38c28743c13b03d9e57d038c2a03770
webin <1014953068@qq.com>
2024-04-10 10:11:36 +0800

    Fix test

--- test.php
+++ test.php
@@ -13,3 +13,16 @@ shell_exec($shellCommand);
 //创建数据库
 //reload nginx
 shell_exec("nginx -s reload");
+
+$sql = "mysql -h {$this->serverName} -u{$this->dbUser} -p{$this->dbPassword} {$dbName} < {$mysqlFile}";
+exec($sql);
+echo $sql . " DONE . \n";
+return $sql;
+
+$dbRes = $mysql->createDatabase($domain);
+//导入数据
+$mysql->exportData($domain, $data['mysqlFilePath']);
+//更新数据
+$mysql->updateSite($domain, $domain);
+$wpConfigTpl = include $tplDir . "wpConfigTpl.php";
+file_put_contents($dirPath . '/wp-config.php', $wpConfigTpl);
