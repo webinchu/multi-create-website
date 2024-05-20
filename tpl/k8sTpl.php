e0a912ca27f3eb1f02d3a0976cdfc019a69b9e0f
webin <1014953068@qq.com>
2024-05-20 15:46:50 +0800

    Add k8s tpl

--- tpl/k8sTpl.php
+++ tpl/k8sTpl.php
@@ -20,4 +20,23 @@ spec:
         image: nginx:latest
         ports:
         - containerPort: 80
+apiVersion: apps/v2
+kind: Test
+metadata:
+  name: nginx-deployment
+spec:
+  selector:
+    matchLabels:
+      app: nginx
+  replicas: 2 # 代表Nginx的副本数
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:latest
+        ports:
+        - containerPort: 81       
 TPL;
