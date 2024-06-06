94ef46c7594a1fcb4944570e61342ffd702af8d3
webin <1014953068@qq.com>
2024-06-06 17:55:00 +0800

    Change tpl

--- tpl/apacheTpl.php
+++ tpl/apacheTpl.php
@@ -1,5 +1,11 @@
 <?php
 return <<<TPL
+
+<VirtualHost *:$portNew>
+     DocumentRoot "$dirPathNew"
+     ServerName $domainNew
+</VirtualHost>
+
 <VirtualHost *:$port>
      DocumentRoot "$dirPath"
      ServerName $domain
