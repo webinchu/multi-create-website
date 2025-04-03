ccd3a3ce23a190d4a09ddb3183a486ff5c037339
webin <1014953068@qq.com>
2025-04-03 11:22:22 +0800

    Del func

--- demo.php
+++ demo.php
@@ -1,3 +1,4 @@
 <?php
 
+//说明参考README
 shell_exec('php sh.php --domains="www.wordpress.test;www.wordpress1.test;www.wordpress1.test" --nginxHostPath=/www/server/panel/vhost/nginx --rootPath=/www/wwwroot --targetDir=wp-tmp --mysqlUser=root --mysqlPwd=root@2020 --dbFilePathmysqlFilePath=/www/demo.sql');
